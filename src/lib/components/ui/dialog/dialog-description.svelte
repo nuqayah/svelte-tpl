7471826a55dfbb4ad20b07f9d7604ebdd53b9060
Mustafa J <mustafa.0x@gmail.com>
2025-03-11 11:48:07 +0300

    add shadcn dialog

---
+++ src/lib/components/ui/dialog/dialog-description.svelte
@@ -0,0 +1,13 @@
+<DialogPrimitive.Description
+    bind:ref
+    class={cn('text-muted-foreground text-sm', className)}
+    {...restProps}
+/>
+
+<script>
+import {Dialog as DialogPrimitive} from 'bits-ui'
+
+import {cn} from '$lib/utils.js'
+
+let {ref = $bindable(null), class: className, ...restProps} = $props()
+</script>
